e6b69c5d8df2787f5a7cccd33f852512fd41d42a
Robson Alves <robson.alves_desenv@hotmail.com>
2015-09-18 14:10:28 -0300

    ngroute and new partial view

--- app/public/js/controllers/ContatoController.js
+++ app/public/js/controllers/ContatoController.js
@@ -1,10 +1,19 @@
+var contatos = [
+	{Codigo: 1, nome: 'Robson Alves', email: 'robsonalves@robsonalves.net', vip: 'true', img:'' },
+	{Codigo: 2, nome: 'Fulano Alves', email: 'robsonalves@robsonalves.net', vip: 'false', img:'' },
+	{Codigo: 3, nome: 'Ciclano Alves', email: 'robsonalves@robsonalves.net', vip: 'false', img:'' },
+	{Codigo: 4, nome: 'Malandro Alves', email: 'robsonalves@robsonalves.net', vip: 'false', img:'' }
+			  ]
+
 angular.module('MEAN')
 	.controller('ContatoController',
 				function($scope){
-			var self = this;
-			this.total = 0;	
-	
-			self.incrementa = function(){
-				this.total++;	
+            $scope.total = 0;     
+	   
+			$scope.incrementa = function(){
+				$scope.total++;
 			};
+    
+            $scope.contatos = contatos;
+            $scope.filtro = '';
 	});
--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,1 +1,18 @@
-angular.module('MEAN',[]);
+angular.module('MEAN',['ngRoute'])
+    .config(function($routeProvider){
+    
+    $routeProvider.when('/contatos',
+                        { templateUrl: 'partials/contatos.html',
+                          controller: 'ContatoController'
+                        });
+    
+$routeProvider.when('/contato/:contatoId',
+                        { templateUrl: 'partials/contato.html',
+                          controller:  'ContatoController'
+                        });
+
+    $routeProvider.otherwise({redirectTo: '/contatos'});
+    
+});
+
+    
